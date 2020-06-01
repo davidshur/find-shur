507411f1feabd6b1dbefed9694266c6096259dd6
David <david@thejurne.com>
2020-05-31 21:33:03 -0700

    add roboto and base landing page

---
+++ src/components/Landing/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Landing = () => (
+  <header className="App-header">
+    <p>
+      Hello world! My name is <span className="my-name">David Shur</span>.
+    </p>
+    <p>
+      I am a full-stack software engineer and web developer.
+    </p>
+  </header>
+);
+
+export default Landing;

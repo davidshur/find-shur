b00157deb49dbb944b9677b5418f85668369173b
David <david@thejurne.com>
2020-06-03 16:21:56 -0700

    update all headers move css to app

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const About = () => (
   <div className="about" name="about">
-    <h3>About David</h3>
+    <h3 className="section-header">About</h3>
     <p>David Shur is a full-stack web developer and software engineer. David is founder and CEO of Jurne where he develops high quality, robust products and leads a team of smart people to achieve their goals together.</p>
     <p>If you would like to learn more about David, you should try one of his steaks, he grills them perfectly!</p>
   </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const Portfolio = () => (
   <div className="portfolio" name="portfolio">
-    <h1>Here are my projects!!</h1>
+    <h3 className="section-header">Projects</h3>
     <div>Project 1</div>
     <div>Project 2</div>
     <div>Project 3</div>

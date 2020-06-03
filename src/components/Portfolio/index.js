fbe08221f19dce83bd71f41976b6bd993fa91fab
David <david@thejurne.com>
2020-06-03 16:29:34 -0700

    semi-ready

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,9 +4,7 @@ import './style.css';
 const Portfolio = () => (
   <div className="portfolio" name="portfolio">
     <h3 className="section-header">Projects</h3>
-    <div>Project 1</div>
-    <div>Project 2</div>
-    <div>Project 3</div>
+    <p className="bio-text">Under construction! Sorry about that...</p>
   </div>
 );
 

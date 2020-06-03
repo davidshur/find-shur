9d76c9a6dcd3a5d48499d61c2435b19ae4e50d87
David <david@thejurne.com>
2020-06-03 16:11:38 -0700

    wrote interim bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './style.css';
 
 const About = () => (
-  <div className="section" name="about">
-    <h1>About Me</h1>
-    <p>My name is David!! I live in Redmond!</p>
+  <div className="about" name="about">
+    <h3>About David</h3>
+    <p>David Shur is a full-stack web developer and software engineer. David is founder and CEO of Jurne where he develops high quality, robust products and leads a team of smart people to achieve their goals together.</p>
+    <p>If you would like to learn more about David, you should try one of his steaks, he grills them perfectly!</p>
   </div>
 );
 

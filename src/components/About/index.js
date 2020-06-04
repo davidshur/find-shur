d5cb20003abb8538e52e797518d04ecf92387987
David <david@thejurne.com>
2020-06-03 20:00:57 -0700

    responsive about done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,18 +9,18 @@ import './style.css';
 const About = () => (
   <div className="about" name="about">
     <h3 className="section-header">About</h3>
-    <div className="cols">
-      <div className="bio">
-        <img className="profile-picture" src={profilePicture} alt="David Shur" />
-        <div className="text-box">
-          <p className="bio-text">I am a full-stack web developer and software engineer. I have a passion for building robust applications that are well integrated. As the founder and CEO of Jurne I have experience in getting projects off the ground quickly. I love building efficient programs and scalable procedures.</p>
-        </div>
+    <div className="bio">
+      <img className="profile-picture" src={profilePicture} alt="David Shur" />
+      <div className="text-box">
+        <p className="bio-text">I am a full-stack web developer and software engineer. I have a passion for building robust applications that are well integrated. As the founder and CEO of Jurne I have experience in getting projects off the ground quickly. I love building efficient programs and scalable procedures.</p>
       </div>
-      <div className="skills">
-        <a className="skill-link" href={resume}><ResumeIcon /></a>
-        <a className="skill-link" href="https://github.com/davidshur"><GithubIcon /></a>
-        <a className="skill-link" href="https://www.linkedin.com/in/david-shur/"><LinkedinIcon /></a>
-      </div>
+    </div>
+    <h5>Get to know me.</h5>
+    <h5><span style={{fontWeight: '500'}}>Click</span> one of the links below!</h5>
+    <div className="skills">
+      <a className="skill-link" href={resume}><ResumeIcon /></a>
+      <a className="skill-link" href="https://github.com/davidshur"><GithubIcon /></a>
+      <a className="skill-link" href="https://www.linkedin.com/in/david-shur/"><LinkedinIcon /></a>
     </div>
   </div>
 );

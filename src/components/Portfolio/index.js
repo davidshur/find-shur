b18797aed7007720a458feb2855fa520bd958b28
David <david@thejurne.com>
2020-06-04 00:02:03 -0700

    add projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Projects from '../../utils/projects';
 import './style.css';
 
-console.log(Projects);
-
 const Portfolio = () => (
   <div className="portfolio" name="portfolio">
     <h3 className="section-header">Projects</h3>
-
+    <div className="project-container">
+      {Projects.map(project =>
+        <div className="project" key={project.id}>
+          <img className="project-image" src={project.image} alt={project.title} />
+        </div>
+      )}
+    </div>
   </div>
 );
 

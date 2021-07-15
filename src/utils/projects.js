3c869f674fceb13c3b70abc4236357fcc72ef483
davidshur <shurdave@gmail.com>
2021-07-15 12:20:41 -0700

    update projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,37 +1,25 @@
 import jurne from '../assets/images/jurne.png';
-import notetaker from '../assets/images/notetaker.png';
-import userdir from '../assets/images/userdir.png';
+import codetalker from '../assets/images/codetalker.png';
+import sendelight from '../assets/images/sendelight.png';
 
 const PROJECTS = [
   {
     id: 0,
     title: 'Jurne',
-    date: '04/26/2020',
-    short: 'Virtual services for car dealerships!',
-    long: 'Jurne allows users to fill out forms from dealerships on line instead of in person!',
-    git: 'https://github.com/thejurne/web-app',
     site: 'https://www.thejurne.com',
     image: jurne,
   },
   {
     id: 1,
-    title: 'Note Taker',
-    date: '2/25/2020',
-    short: 'Take notes and save them for later!',
-    long: 'Take notes that will persist long after you leave the site.',
-    git: 'https://github.com/davidshur/note-taker',
-    site: 'https://note-taker-29587.herokuapp.com/',
-    image: notetaker,
+    title: 'Sudo Code Search',
+    site: 'https://www.codetalker.io',
+    image: codetalker,
   },
   {
     id: 2,
-    title: 'User Directory',
-    date: '04/26/2020',
-    short: 'Search through your users!',
-    long: 'Typing in the search box will allow you to search for the users you want.',
-    git: 'https://github.com/davidshur/user-dir',
-    site: 'https://user-dir-39127.herokuapp.com/',
-    image: userdir,
+    title: 'Sendelight',
+    site: 'https://www.sendelight.com',
+    image: sendelight,
   }
 ];
 
